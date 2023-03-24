47c5325495d3e16f4189a12655a33ad99d8672d2
DonzerHD <thomas.lemay59@outlook.fr>
2023-03-24 11:41:15 +0100

    ajout docker

--- src/authentication/forms.py
+++ src/authentication/forms.py
@@ -2,30 +2,53 @@ from django import forms
 from django.contrib.auth.models import User
 
 
-
 class LoginForm(forms.Form):
+    """
+    A form for user login
+    """
     username = forms.CharField(label="Username")
     password = forms.CharField(label="Password", widget=forms.PasswordInput)
-    
+
+
 class RegisterForm(forms.Form):
+    """
+    A form for user registration
+    """
     username = forms.CharField(label="Username")
     password1 = forms.CharField(label="Password", widget=forms.PasswordInput)
     password2 = forms.CharField(label="Password Confirmation", widget=forms.PasswordInput)
     email = forms.EmailField(label="Email")
     
     def clean_username(self):
+        """
+        Validate that the username is unique
+        """
+        # Retrieve the username from cleaned data
         username = self.cleaned_data['username']
+        # Check if a user with the same username already exists in the database
         if User.objects.filter(username=username).exists():
+            # Raise a validation error if a user with the same username exists
             raise forms.ValidationError("Username already used")
+        # Return the cleaned username
         return username
     
     def clean_email(self):
+        """
+        Validate that the email is unique
+        """
+        # Retrieve the email from cleaned data
         email = self.cleaned_data['email']
+        # Check if a user with the same email already exists in the database
         if User.objects.filter(email=email).exists():
+            # Raise a validation error if a user with the same email exists
             raise forms.ValidationError("Email already used")
+        # Return the cleaned email
         return email
     
     def clean(self):
+        """
+        Validate that the passwords match
+        """
         cleaned_data = super().clean()
         password1 = cleaned_data.get("password1")
         password2 = cleaned_data.get("password2")
